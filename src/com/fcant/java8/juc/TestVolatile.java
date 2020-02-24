a86a65b5d153e59d7d6ef975ba3d309076c71ff3
Fcant <fcscanf@outlook.com>
2020-02-24 17:28:32 +0800

    Java8新特性-JUC-使用volatile关键字进行主存变量的数据共享

--- src/com/fcant/java8/juc/TestVolatile.java
+++ src/com/fcant/java8/juc/TestVolatile.java
@@ -5,7 +5,11 @@ package com.fcant.java8.juc;
  * <p>
  * encoding:UTF-8
  *
- * volatile关键字
+ * volatile关键字：当多个线程操作数据时，可以保证内存中的数据的可见性
+ *                相较于synchronize是一种较为轻量级的同步策略
+ *  注意：
+ *  1.volatile 不具备“互斥性”
+ *  2.volatile 不能保证变量的“原子性”
  *
  * @author Fcant 下午 12:49:36 2020/2/24/0024
  */
@@ -15,18 +19,16 @@ public class TestVolatile {
         new Thread(fh).start();
 
         while (true) {
-            synchronized (fh) {
-                if (fh.isFlag()) {
-                    System.out.println("-----------------");
-                    break;
-                }
+            if (fh.isFlag()) {
+                System.out.println("-----------------");
+                break;
             }
         }
     }
 }
 
 class ThreadFh implements Runnable {
-    private boolean flag = false;
+    private volatile boolean flag = false;
 
     public boolean isFlag() {
         return flag;
