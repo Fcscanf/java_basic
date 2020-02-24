6380aaec8f1c303743cea1324db9b6671ff02dd2
Fcant <fcscanf@outlook.com>
2020-02-24 16:52:10 +0800

    Java8新特性-JUC-主存的共享数据在更改后其他线程拿到的数据未更新，即内存不可见性

---
+++ src/com/fcant/java8/juc/TestVolatile.java
@@ -0,0 +1,47 @@
+package com.fcant.java8.juc;
+
+/**
+ * TestVolatile
+ * <p>
+ * encoding:UTF-8
+ *
+ * volatile关键字
+ *
+ * @author Fcant 下午 12:49:36 2020/2/24/0024
+ */
+public class TestVolatile {
+    public static void main(String[] args) {
+        ThreadFh fh = new ThreadFh();
+        new Thread(fh).start();
+
+        while (true) {
+            if (fh.isFlag()) {
+                System.out.println("-----------------");
+                break;
+            }
+        }
+    }
+}
+
+class ThreadFh implements Runnable {
+    private boolean flag = false;
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        flag = true;
+        System.out.println("flag=" + isFlag());
+    }
+}
