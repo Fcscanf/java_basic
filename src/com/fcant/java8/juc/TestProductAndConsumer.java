ada118b6073780305cd5e035868ad781765c4dfa
Fcant <fcscanf@outlook.com>
2020-02-25 11:32:58 +0800

    Java8新特性-JUC-生产者消费者演示

---
+++ src/com/fcant/java8/juc/TestProductAndConsumer.java
@@ -0,0 +1,86 @@
+package com.fcant.java8.juc;
+
+/**
+ * TestProductorAndConsumer
+ * <p>
+ * encoding:UTF-8
+ *
+ * @author Fcant 上午 10:46:52 2020/2/25/0025
+ */
+public class TestProductAndConsumer {
+    public static void main(String[] args) {
+        Clerk clerk = new Clerk();
+        Product product = new Product(clerk);
+        Consumer consumer = new Consumer(clerk);
+
+        new Thread(product, "生产者A").start();
+        new Thread(consumer, "消费者B").start();
+    }
+}
+
+// 店员
+class Clerk{
+    private int product = 0;
+
+    // 进货
+    public synchronized void get() {
+        if (product >= 10) {
+            System.out.println("仓库已满，无法进货！");
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println(Thread.currentThread().getName() + " : " + ++product);
+            this.notifyAll();
+        }
+    }
+
+    // 卖货
+    public synchronized void sale() {
+        if (product <= 0) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("缺货中···");
+        } else {
+            System.out.println(Thread.currentThread().getName() + " : " + --product);
+            this.notifyAll();
+        }
+    }
+}
+
+// 生产者
+class Product implements Runnable{
+    private Clerk clerk;
+
+    public Product(Clerk clerk) {
+        this.clerk = clerk;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 20; i++) {
+            clerk.get();
+        }
+    }
+}
+
+// 消费者
+class Consumer implements Runnable{
+    private Clerk clerk;
+
+    public Consumer(Clerk clerk) {
+        this.clerk = clerk;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 20; i++) {
+            clerk.sale();
+        }
+    }
+}
